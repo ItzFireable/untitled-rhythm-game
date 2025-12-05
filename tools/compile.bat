881398db90d21cb443c8ecc3c4cab6835a2040fd
ItzFireable <fireableplayz@gmail.com>
2025-12-05 19:40:15 +0200

    Fix compiling and update tools

--- tools/compile.bat
+++ tools/compile.bat
@@ -1,4 +1,2 @@
-cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=DEBUG-S . -B output -DCMAKE_CXX_FLAGS_DEBUG_INIT="-fsanitize=undefined -fsanitize-trap -std=c++20" -DCMAKE_SHARED_LINKER_FLAGS_DEBUG_INIT="-l:libstdc++.a"
-
 cd output
-cmake --build .
+cmake --build . --verbose
